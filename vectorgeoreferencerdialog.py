357c1662f082ebf45149ad09ec217cd5b7fd7af7
francescomarucci <francesco.marucci@gmail.com>
2017-05-21 14:31:29 +0200

    Add files via upload

--- vectorgeoreferencerdialog.py
+++ vectorgeoreferencerdialog.py
@@ -13,10 +13,11 @@ from vectorgeoreferencertransformers import *
 
 
 class VectorGeoreferencerDialog(QWidget):
-    def __init__(self, iface, vb):
+    def __init__(self, iface, vb, parent = None):
+        super(VectorGeoreferencerDialog, self).__init__(parent)
         QWidget.__init__(self)
         uic.loadUi(os.path.join(os.path.dirname(__file__),'ui_main_new.ui'), self)
-        self.setFocusPolicy(Qt.ClickFocus)
+        self.setFocusPolicy(Qt.StrongFocus)
         #self.setWindowModality( Qt.ApplicationModal )
 
         self.iface = iface
@@ -40,8 +41,15 @@ class VectorGeoreferencerDialog(QWidget):
 
         # Create an event filter to update on focus
         self.installEventFilter(self)
-
-
+    
+      
+    # Events
+    def eventFilter(self,object,event):
+        if QEvent is not None:
+          if event.type() == QEvent.WindowActivate:
+            self.checkRequirements()
+        return False      
+      
     # UI Getters
     def toGeorefLayer(self):
         """
@@ -80,142 +88,158 @@ class VectorGeoreferencerDialog(QWidget):
         # Checkin requirements
         self.runButton.setEnabled(False)
 
+        while (self.tableParams.rowCount() > 0):
+          self.tableParams.removeRow(0);
+        while (self.tableWidget.rowCount() > 0):
+          self.tableWidget.removeRow(0);
+        self.textEdit.setText("")
+        
         tbl = self.toGeorefLayer()
-        
         pl = self.pairsLayer()
-        restrictToSelection=self.restrictBox_pairsLayer.isChecked()
         
-        if pl is not None:
-          if pl.geometryType() == QGis.Line:
-            features = pl.allFeatureIds() if not restrictToSelection else pl.selectedFeaturesIds()
-            totFeat=len(features)
-            if(totFeat>3 and totFeat<100):
-              #QgsMessageLog.logMessage(str(len(features)), 'Vector Georeferencer Plugin')
-              
-              pointsA = []
-              pointsB = []
-              features = pl.getFeatures() if not restrictToSelection else pl.selectedFeatures()
-
-              for feature in features:
-                geom = feature.geometry().asPolyline()
-                pointsA.append( QgsPoint(geom[0].x(),geom[0].y()) )
-                pointsB.append( QgsPoint(geom[-1].x(),geom[-1].y()) )
-
-              self.transformer = AffineTransformer(pl, restrictToSelection)
-
-              while (self.tableParams.rowCount() > 0):
-                self.tableParams.removeRow(0);
-                
-              affineParams=self.transformer.calculateAffineParams(pointsA,pointsB)
-              a=affineParams[0]
-              b=affineParams[1]
-              c=affineParams[2]
-              d=affineParams[3]
-              tx=affineParams[4]
-              ty=affineParams[5]
-              #QgsMessageLog.logMessage(" a: "+str(a)+"\n b: "+str(b)+"\n c: "+str(c)+"\n d: "+str(d)+"\n tx: "+str(tx)+"\n ty: "+str(ty), 'Vector Georeferencer Plugin')
-              #msg= " a: "+str(a)+"\n b: "+str(b)+"\n c: "+str(c)+"\n d: "+str(d)+"\n tx: "+str(tx)+"\n ty: "+str(ty) +"\n"
-
-
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("a"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(a)))          
-
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("b"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(b)))  
-              
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("c"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(c)))              
-
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("d"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(d)))  
-              
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("tx"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(tx)))  
-              
-              rowPosition = self.tableParams.rowCount()
-              self.tableParams.insertRow(rowPosition)
-              self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("ty"))
-              self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(ty))) 
-              
-              header = self.tableParams.horizontalHeader()
-              header.setResizeMode(1, QHeaderView.ResizeToContents)
-              
-              features = pl.getFeatures() if not restrictToSelection else pl.selectedFeatures()
-              rsmd=0
-              
-              while (self.tableWidget.rowCount() > 0):
-                self.tableWidget.removeRow(0);
-
-              rmsA = []
-              ids=[]
-              for feature in features:
-                geom = feature.geometry().asPolyline()
-                p=QgsPoint(geom[0].x(),geom[0].y())
-                newListA = []
-                newListA.append( self.transformer.map(p) )
-                newListA.append( QgsPoint(geom[-1].x(),geom[-1].y()) )
-                newGeom = QgsGeometry.fromPolyline( newListA )
-                rsmd+=newGeom.length()**2
-                rmsA.append(newGeom.length())
-                attrs = feature.attributes()
-                ids.append(attrs[0])
-              
-              rsmd=math.sqrt(rsmd/totFeat)
-              for t in range(totFeat):  
-                rowPosition = self.tableWidget.rowCount()
-                self.tableWidget.insertRow(rowPosition)
-                self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(str(ids[t])))
-                it=QTableWidgetItem(str(rmsA[t]))
-                if rmsA[t]>rsmd:
-                  it.setTextColor(QColor(216, 50, 0))
-                self.tableWidget.setItem(rowPosition, 1, it)
-              
-              header = self.tableWidget.horizontalHeader()
-              #header.setResizeMode(0, QHeaderView.ResizeToContents)            
-              #header.setResizeMode(1, QHeaderView.ResizeToContents)
-              
-              if(rsmd>=1):
-                self.textEdit.setText(" Root Mean Square Deviation: "+"<font color='red'><b>"+str(rsmd)+"</b></font> calculated on <b>"+str(totFeat)+"</b> control points.<br />Please add control points or remove the control points with high RMS (the red ones).")
-              else:
-                self.textEdit.setText(" Root Mean Square Deviation: <b>"+str(rsmd)+"</b> calculated on <b>"+str(totFeat)+"</b> control points.")
-              pl.loadNamedStyle(os.path.join(os.path.dirname(__file__),'PairStyle.qml'), False)
-              pl.repaintRequested.emit()
-              
-            else:
-              while (self.tableWidget.rowCount() > 0):
-                self.tableWidget.removeRow(0);    
-              while (self.tableParams.rowCount() > 0):
-                self.tableParams.removeRow(0);
-              if restrictToSelection:
-                self.textEdit.setText(" Please select more than 3 control points in the Pairs layer")
-              else:
-                self.textEdit.setText(" Please chose a Pairs layer with more than 3 control points")  
-              
         if tbl is None:
-            self.displayMsg( "You must select a vector layer to georeference !", True )
-            return
+          self.displayMsg( "You must select a vector layer to georeference !", True )
+          return
         if pl is None:
-            self.displayMsg( "You must select a vector (line) layer which defines the points pairs !", True )
-            return
+          self.displayMsg( "You must select a vector (line) layer which defines the control points pairs !", True )
+          return
+
+        if len(tbl.selectedFeaturesIds())>0:
+          self.restrictBox_toGeorefLayer.setEnabled(True)
+        else:
+          self.restrictBox_toGeorefLayer.setEnabled(True)
+          self.restrictBox_toGeorefLayer.setChecked(False)
+          self.restrictBox_toGeorefLayer.setEnabled(False)
+          
+        if len(pl.selectedFeaturesIds())>0:
+          self.restrictBox_pairsLayer.setEnabled(True)
+        else:
+          self.restrictBox_pairsLayer.setEnabled(True)
+          self.restrictBox_pairsLayer.setChecked(False)
+          self.restrictBox_pairsLayer.setEnabled(False)
+        
+        restrictToSelection=self.restrictBox_pairsLayer.isChecked()
+
+        if pl.geometryType() == QGis.Line:
+          
+          features = pl.allFeatureIds() if not restrictToSelection else pl.selectedFeaturesIds()
+          totFeat=len(features)
+          if(totFeat>3 and totFeat<100):
+            
+            pointsA = []
+            pointsB = []
+            features = pl.getFeatures() if not restrictToSelection else pl.selectedFeatures()
+
+            for feature in features:
+              geom = feature.geometry().asPolyline()
+              pointsA.append( QgsPoint(geom[0].x(),geom[0].y()) )
+              pointsB.append( QgsPoint(geom[-1].x(),geom[-1].y()) )
+
+            self.transformer = AffineTransformer(pl, restrictToSelection)
+
+            while (self.tableParams.rowCount() > 0):
+              self.tableParams.removeRow(0);
+              
+            affineParams=self.transformer.calculateAffineParams(pointsA,pointsB)
+            a=affineParams[0]
+            b=affineParams[1]
+            c=affineParams[2]
+            d=affineParams[3]
+            tx=affineParams[4]
+            ty=affineParams[5]
+            #QgsMessageLog.logMessage(" a: "+str(a)+"\n b: "+str(b)+"\n c: "+str(c)+"\n d: "+str(d)+"\n tx: "+str(tx)+"\n ty: "+str(ty), 'Vector Georeferencer Plugin')
+ 
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("a"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(a)))          
+
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("b"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(b)))  
+            
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("c"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(c)))              
+
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("d"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(d)))  
+            
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("tx"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(tx)))  
+            
+            rowPosition = self.tableParams.rowCount()
+            self.tableParams.insertRow(rowPosition)
+            self.tableParams.setItem(rowPosition, 0, QTableWidgetItem("ty"))
+            self.tableParams.setItem(rowPosition, 1, QTableWidgetItem(str(ty))) 
+            
+            header = self.tableParams.horizontalHeader()
+            header.setResizeMode(1, QHeaderView.ResizeToContents)
+            
+            features = pl.getFeatures() if not restrictToSelection else pl.selectedFeatures()
+            rsmd=0
+            
+            while (self.tableWidget.rowCount() > 0):
+              self.tableWidget.removeRow(0);
+
+            rmsA = []
+            ids=[]
+            for feature in features:
+              geom = feature.geometry().asPolyline()
+              p=QgsPoint(geom[0].x(),geom[0].y())
+              newListA = []
+              newListA.append( self.transformer.map(p) )
+              newListA.append( QgsPoint(geom[-1].x(),geom[-1].y()) )
+              newGeom = QgsGeometry.fromPolyline( newListA )
+              rsmd+=newGeom.length()**2
+              rmsA.append(newGeom.length())
+              attrs = feature.attributes()
+              ids.append(attrs[0])
+            
+            rsmd=math.sqrt(rsmd/totFeat)
+            for t in range(totFeat):  
+              rowPosition = self.tableWidget.rowCount()
+              self.tableWidget.insertRow(rowPosition)
+              self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(str(ids[t])))
+              it=QTableWidgetItem(str(rmsA[t]))
+              if rmsA[t]>rsmd:
+                it.setTextColor(QColor(216, 50, 0))
+              self.tableWidget.setItem(rowPosition, 1, it)
+            
+            header = self.tableWidget.horizontalHeader()
+            #header.setResizeMode(0, QHeaderView.ResizeToContents)            
+            #header.setResizeMode(1, QHeaderView.ResizeToContents)
+            
+            if(rsmd>=1):
+              self.textEdit.setText(" Root Mean Square Deviation: "+"<font color='red'><b>"+str(rsmd)+"</b></font> calculated on <b>"+str(totFeat)+"</b> control points.<br />Please add control points or remove the control points with high RMS (the red ones).")
+            else:
+              self.textEdit.setText(" Root Mean Square Deviation: <b>"+str(rsmd)+"</b> calculated on <b>"+str(totFeat)+"</b> control points.")
+            pl.loadNamedStyle(os.path.join(os.path.dirname(__file__),'PairStyle.qml'), False)
+            pl.repaintRequested.emit()
+            
+          else:
+            while (self.tableWidget.rowCount() > 0):
+              self.tableWidget.removeRow(0);    
+            while (self.tableParams.rowCount() > 0):
+              self.tableParams.removeRow(0);
+            if restrictToSelection:
+              self.textEdit.setText(" Please select more than 3 control points in the Pairs layer")
+            else:
+              self.textEdit.setText(" Please chose a Pairs layer with more than 3 control points")  
+              
+
         if pl is tbl:
             self.displayMsg( "The layer to georeference must be different from the pairs layer !", True )
             return            
         if not tbl.isEditable():
             self.displayMsg( "The layer to georeference must be in edit mode !", True )
             return
-        #if self.stackedWidget.currentIndex() == 0:
-        #    self.displayMsg("Impossible to run with an invalid transformation type.", True)
-        #    return
   
         self.displayMsg("Ready to go...")
         self.runButton.setEnabled(True)
@@ -250,12 +274,6 @@ class VectorGeoreferencerDialog(QWidget):
         self.editModeButton_toGeorefLayer.setChecked( False if (l is None or not l.isEditable()) else True )
         
     def updateTransformationType(self):
-        """
-        Update the stacked widget to display the proper transformation type. Also runs checkRequirements() 
-        """
-        #tt = self.vb.determineTransformationType()
-        #self.stackedWidget.setCurrentIndex( tt )
-
         self.checkRequirements()
 
     # Togglers
@@ -308,14 +326,8 @@ class VectorGeoreferencerDialog(QWidget):
             #QApplication.beep()
             msg = "<font color='red'>"+msg+"</font>"
         self.statusLabel.setText( msg ) 
+
         
-    def displayParMsg(self, msg):
-        self.parametersLabel.setText( msg )  
-        
-    # Events
-    def eventFilter(self,object,event):
-        #if event.type() == QEvent.FocusIn:
-        #self.refreshStates()
-        return False
+
 
 
